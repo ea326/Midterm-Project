10d96fe272d1b148d8cd708e58f93d698b2fab50
Eric Alfajora <ea326@njit.edu>
2025-06-28 18:17:18 -0400

    Implement undo method to revert last calculation

--- app/history.py
+++ app/history.py
@@ -6,3 +6,10 @@ class History:
     def add(self, calculation):
         self._history.append(calculation)
         self._redo_stack.clear()
+
+    def undo(self):
+        if not self._history:
+            raise IndexError("No operations to undo.")
+        calc = self._history.pop()
+        self._redo_stack.append(calc)
+        return calc
