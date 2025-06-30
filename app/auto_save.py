579fa341fa85e85f37d266d414ec229aa8607f15
Eric Alfajora <ea326@njit.edu>
2025-06-30 16:58:33 -0400

    Use CALCULATOR_HISTORY_DIR and CALCULATOR_AUTO_SAVE in AutoSaveObserver

--- app/auto_save.py
+++ app/auto_save.py
@@ -1,24 +1,24 @@
+import os
 import pandas as pd
 from app.observer import Observer
+from app.calculator_config import CALCULATOR_HISTORY_DIR, CALCULATOR_AUTO_SAVE
 
 class AutoSaveObserver(Observer):
-    def __init__(self, file_path='history.csv'):
-        self.file_path = file_path
+    def __init__(self):
+        os.makedirs(CALCULATOR_HISTORY_DIR, exist_ok=True)
+        self.file_path = os.path.join(CALCULATOR_HISTORY_DIR, "history.csv")
 
     def update(self, calculation):
-        data = [{
-            'operation': calculation.operation.__class__.__name__,
-            'a': calculation.a,
-            'b': calculation.b,
-            'result': calculation.result,
-            'timestamp': calculation.timestamp
-        }]
-        df = pd.DataFrame(data)
-        df.to_csv(self.file_path, mode='a', index=False, header=not self._file_exists())
+        if not CALCULATOR_AUTO_SAVE:
+            return
 
-    def _file_exists(self):
-        try:
-            with open(self.file_path, 'r'):
-                return True
-        except FileNotFoundError:
-            return False
+        from app.history import history  # delayed to avoid circular import
+        data = [{
+            'operation': calc.operation.__class__.__name__,
+            'a': calc.a,
+            'b': calc.b,
+            'result': calc.result,
+            'timestamp': calc.timestamp
+        } for calc in history.get_history()]
+        df = pd.DataFrame(data)
+        df.to_csv(self.file_path, index=False)
